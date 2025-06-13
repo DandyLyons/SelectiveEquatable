febeb44f464e87e6e252b247cc38535d00a09499
Daniel Lyons <72824209+DandyLyons@users.noreply.github.com>
2025-06-13 15:14:20 -0600

    Add tests

--- Tests/SelectiveEquatableTests/SelectiveEquatableTests.swift
+++ Tests/SelectiveEquatableTests/SelectiveEquatableTests.swift
@@ -1,6 +1,23 @@
 import Testing
 @testable import SelectiveEquatable
 
-@Test func example() async throws {
+final class ExampleStruct: SelectiveEquatable {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+@Test func isEqual() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let example1 = ExampleStruct(name: "Mickey", age: 100)
+    let example2 = ExampleStruct(name: "Mickey", age: 200)
+    
+    #expect(example1.name == example2.name)
+    #expect(example1.age != example2.age)
+    #expect(example1.isEqual(to: example2, by: \.name))
+    #expect(example1.isEqual(to: example2, by: \.age) != true)
 }
